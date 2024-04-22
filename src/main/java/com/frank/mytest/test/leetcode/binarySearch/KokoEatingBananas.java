e54962c92ac405ef763b7b00f018a27852152f3e
FrankHuang <frank.huang@kooppi.com>
2024-04-22 22:34:12 +0800

    feat: new code

---
+++ src/main/java/com/frank/mytest/test/leetcode/binarySearch/KokoEatingBananas.java
@@ -0,0 +1,52 @@
+package com.frank.mytest.test.leetcode.binarySearch;
+
+/**
+ * Example 1:
+ * Input: piles = [3,6,7,11], h = 8
+ * Output: 4
+ * <p>
+ * Example 2:
+ * Input: piles = [30,11,23,4,20], h = 5
+ * Output: 30
+ * <p>
+ * Example 3:
+ * Input: piles = [30,11,23,4,20], h = 6
+ * Output: 23
+ */
+public class KokoEatingBananas {
+    /**
+     * 給定一 array 代表多串香蕉, arr[i] 為每串香蕉根數
+     * Koko 每次只能拿一串，並可以吃 k 根/小時，若 arr[i] < k 則依舊會消耗一小時。
+     * 問在 h 小時內，最慢 k 需為多少才能把所有香蕉吃完
+     */
+    public static void main(String[] args) {
+        int[] piles = new int[]{30, 11, 23, 4, 20};
+        System.out.println(minEatingSpeed(piles, 5));
+    }
+
+    public static int minEatingSpeed(int[] piles, int h) {
+        int max = piles[0];
+        for (int n : piles) {
+            max = Math.max(max, n);
+        }
+        int l = 1;
+        int r = max; // 最小速度必定在 1 ... max 之間
+        int minSpeed = Integer.MAX_VALUE;
+        while (l <= r) {
+            long currHour = 0; // 需要時間太長可能導致整數 overflow，因此用 long
+            int mid = l + (r - l) / 2; // 取中心點的速度並計算總共需多少時間
+            for (int p : piles) {
+                // ceil 運算需為 double
+                long time = (long) Math.ceil((double) p / (double) mid);
+                currHour += time;
+            }
+            if (currHour <= h) { // 在時限內都算可以的結果
+                minSpeed = Math.min(minSpeed, mid);
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return minSpeed;
+    }
+}
---
+++ src/main/java/com/frank/mytest/test/leetcode/stack/DailyTemperature.java
@@ -0,0 +1,31 @@
+package com.frank.mytest.test.leetcode.stack;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+/**
+ * 給定一 array 表示每天的溫度，回傳一 array 表示第 i 天遇到下一次更溫暖的日子需要幾天，若沒有則填入 0
+ * Example 1:
+ * Input: temperatures = [73,74,75,71,69,72,76,73]
+ * Output: [1,1,4,2,1,1,0,0]
+ */
+public class DailyTemperature {
+    public static void main(String[] args) {
+        int[] temps = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
+        System.out.println(Arrays.toString(dailyTemperatures(temps)));
+    }
+
+    public static int[] dailyTemperatures(int[] temps) {
+        int[] res = new int[temps.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < temps.length; i++) {
+            while (!stack.isEmpty() && temps[stack.peek()] < temps[i]) {
+                Integer pop = stack.pop();
+                res[pop] = i - pop;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
